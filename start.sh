39a9b077295529638ab6422d105601973207e8a1
shahar05 <shram92@gmail.com>
2023-04-18 14:42:38 +0300

    sad

--- start.sh
+++ start.sh
@@ -9,8 +9,10 @@ ls /app
 echo "$DB_SOURCE"
 source /app/app.env
 source app.env
+echo "222222"
+cat app.env
 echo $DB_SOURCE
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+/app/migrate -path /app/migration -database postgresql://postgres:7tghyMSFFqbZOLeF0s1m@simple-bank1.cesczdjwvygy.eu-west-1.rds.amazonaws.com:5432/simple_bank -verbose up
 
 echo "start the app"
 exec "$@"
