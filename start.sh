76198b807bc3b15e27275d211b7d95acfbda549d
shahar05 <shram92@gmail.com>
2023-04-18 14:07:22 +0300

    start

--- start.sh
+++ start.sh
@@ -3,8 +3,9 @@
 set -e
 
 echo "run db migrations"
-echo "asdasdas"
-cat /app/app.env
+ls
+ls /app
+echo "asdasd"
 source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
