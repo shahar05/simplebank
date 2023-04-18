847d3937c72151e37a451154bb68034a429c00af
shahar05 <shram92@gmail.com>
2023-04-18 14:14:37 +0300

    another commit

--- start.sh
+++ start.sh
@@ -4,9 +4,12 @@ set -e
 
 echo "run db migrations"
 ls
+echo "1111111"
 ls /app
-echo "asdasd"
+echo "$DB_SOURCE"
 source /app/app.env
+source app.env
+echo $DB_SOURCE
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
