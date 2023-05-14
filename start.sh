6bd06299043bb76a472dfca043dac1ef591884b7
shahar05 <shram92@gmail.com>
2023-05-14 11:41:49 +0300

    add echo

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 set -e
-
 echo "run db migrations"
 source /app/app.env
 echo "before migrate"
 cat /app/app.env
 ls /app
+echo "$DB_SOURCE"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 echo "after migrate"
 
